c2935efc8e62a9d2cf8531197ec56494e7ff0735
manikanta-1322 <allatiapallimanikantareddy@gmail.com>
2024-08-26 17:43:15 +0530

    Pushing Roboshop Ansible

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "env" {}
+variable "tags" {}
+variable "component" {}
+variable "instance_type" {}
+variable "instance_count" {}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "app_port" {}
+variable "sg_cidrs" {}
+variable "bastion_cidrs" {}
+variable "kms" {}
+variable "prometheus_cidrs" {}
+variable "alb_name" {}
+variable "listener_arn" {}
+variable "route53_zone_id" {}
+variable "priority" {}
+variable "dns_name" {}
